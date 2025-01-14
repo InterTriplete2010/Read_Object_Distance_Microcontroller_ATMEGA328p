ae97628a1c4e8e5634fded508fd64e6944ff5b55
Ale Presacco <pressalex@hotmail.com>
2025-01-14 14:10:19 -0800

    Create Arduino.cpp

---
+++ Arduino.cpp
@@ -0,0 +1,26 @@
+void setup() 
+{
+
+  Serial.begin(9600); //initialize serial communication at a 9600 baud rate
+
+}
+
+void loop() {
+
+Serial.write(66);
+
+  delay(1000)
+  
+  if (Serial.available() > 0) 
+  {
+  
+
+     Serial.print(" - I received: ");
+
+    Serial.print(Serial.read());
+
+Serial.println(" cm");
+
+}
+
+}
